e2ecbe006730fe86e36805a8de6288d6ce650b8a
wlghd28 <hojindohong@nate.com>
2020-02-25 12:12:01 +0900

    data.js 수정

--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,7 @@ const   cron        = require('node-cron');
 const   mysql       = require('mysql');
 const   express     = require('express');
 const   router      = express.Router();
-
+const   async       = require('async');
 /* 
     데이터베이스 연동 소스코드 
 */
@@ -18,30 +18,17 @@ const db = mysql.createConnection({
     토요일에서 일요일이 넘어가는 자정이 될 경우 데이터를 백업합니다.
 */
 const DataBackup = (req, res) => {
-    cron.schedule('0, 0, 0, *, *, 0', () => {
+    //cron.schedule('0, 0, 0, *, *, 0', () => {
         // 금주 주업무 데이터를 지난 업무 데이터로 백업 시키는 과정
         let sql_str1 = "SELECT * FROM THIS_WORK";
         let sql_str2 = "INSERT INTO LAST_WORK(start_date, end_date, user_id, user_name, work) VALUES(?, ?, ?, ?, ?)";
         let sql_str3 = "DELETE FROM THIS_WORK";
 
-        /*
-        // 예정된 주업무 데이터를 금주 업무로 업데이트 시키는 과정
-        let sql_str4 = "SELECT * FROM FUTUER_WORK";
-        let sql_str5 = "INSERT INTO THIS_WORK(start_date, end_date, user_id, work) VALUES(?, ?, ?, ?)";
-        let sql_str6 = "DELETE FROM FUTURE_WORK";
-        */ 
 
         // 금주 부업무 데이터를 지난 업무 데이터로 백업 시키는 과정
         let sql_str7 = "SELECT * FROM SUB_THIS_WORK";
         let sql_str8 = "INSERT INTO SUB_LAST_WORK(start_date, end_date, user_id, user_name, work) VALUES(?, ?, ?, ?, ?)";
         let sql_str9 = "DELETE FROM SUB_THIS_WORK";
-
-        /*
-        // 예정된 부업무 데이터를 금주 업무로 업데이트 시키는 과정
-        let sql_str10 = "SELECT * FROM SUB_FUTUER_WORK";
-        let sql_str11 = "INSERT INTO SUB_THIS_WORK(start_date, end_date, user_id, work) VALUES(?, ?, ?, ?)";
-        let sql_str12 = "DELETE FROM SUB_FUTURE_WORK";
-        */
 
         async.waterfall([
             function(callback) {
@@ -51,27 +38,17 @@ const DataBackup = (req, res) => {
                         console.log(error);
                         res.end("error");
                     } else {
-                        if (results.length > 0) {
-                            for (var i = 0; i < results.length ; i++) {
-                                db.query(sql_str2, [results[i].start_date, results[i].end_date, 
-                                    results[i].user_id, results[i].user_name, results[i].work], (error) => {
-                                    if (error) {
-                                        console.log(error);
-                                        res.end("error");
-                                    } else {
-                                        console.log("LAST_WORK data was inserted...");
-                                    }
-                                });
-                            }
+                        for (var i = 0; i < results.length ; i++) {
+                            db.query(sql_str2, [results[i].start_date, results[i].end_date, 
+                                results[i].user_id, results[i].user_name, results[i].work], (error) => {
+                                if (error) {
+                                    console.log(error);
+                                    res.end("error");
+                                } else {
+                                    console.log("LAST_WORK data was inserted...");
+                                }
+                            });
                         }
-                        db.query(sql_str3, (error) => {
-                            if (error) {
-                                console.log(error);
-                                res.end("error");
-                            } else {
-                               console.log("THIS_WORK data was deleted...");
-                            }
-                        });
                     }
                 });
                 // 금주 부업무 데이터를 지난 업무 데이터로 백업 시키는 과정
@@ -80,99 +57,46 @@ const DataBackup = (req, res) => {
                         console.log(error);
                         res.end("error");
                     } else {
-                        if (results.length > 0) {
-                            for(var i = 0; i < results.length; i++) {
-                                db.query(sql_str8, [results[i].start_date, results[i].end_date, 
-                                    results[i].user_id, results[i].user_name, results[i].work], (error) => {
-                                    if (error) {
-                                        console.log(error);
-                                        res.end("error");
-                                    } else {
-                                        console.log("SUB_LAST_WORK data was inserted...");
-                                    }
-                                });
-                            }
-                        }
-                        db.query(sql_str9, (error) => {
-                            if (error) {
-                                console.log(error);
-                                res.end("error");
-                            } else {
-                               console.log("SUB_THIS_WORK data was deleted...");
-                            }
-                        });
+                        for(var i = 0; i < results.length; i++) {
+                            db.query(sql_str8, [results[i].start_date, results[i].end_date, 
+                                results[i].user_id, results[i].user_name, results[i].work], (error) => {
+                                if (error) {
+                                    console.log(error);
+                                    res.end("error");
+                                } else {
+                                    console.log("SUB_LAST_WORK data was inserted...");
+                                }
+                            });
+                        }                      
                     }
                 });
                 callback(null);
             },
-            /*
-            function(callback) {
-                // 예정된 주업무 데이터를 금주 업무로 업데이트 시키는 과정
-                db.query(sql_str4, (error, results) => {
+            // 금주 주업무, 부업무 테이블에 있는 데이터를 삭제합니다.
+            function(callback){
+                db.query(sql_str3, (error) => {
                     if (error) {
                         console.log(error);
                         res.end("error");
                     } else {
-                        if(results.length > 0) {
-                            for(var i = 0; i < results.length; i++) {
-                                db.query(sql_str5, [results[i].start_date, results[i].end_date, 
-                                    results[i].user_id, results[i].work], (error) => {
-                                    if (error) {
-                                        console.log(error);
-                                        res.end("error");
-                                    } else {
-                                        console.log("THIS_WORK data was inserted...");
-                                    }
-                                });
-                            }
-                        }
-                        db.query(sql_str6, (error) => {
-                            if (error) {
-                                console.log(error);
-                                res.end("error");
-                            } else {
-                               console.log("FUTURE_WORK data was deleted...");
-                            }
-                        });
+                    console.log("THIS_WORK data was deleted...");
                     }
                 });
-                // 예정된 부업무 데이터를 금주 업무로 업데이트 시키는 과정
-                db.query(sql_str10, (error, results) => {
+                db.query(sql_str9, (error) => {
                     if (error) {
                         console.log(error);
                         res.end("error");
                     } else {
-                        if(results.length > 0) {
-                            for(var i = 0; i < results.length; i++) {
-                                db.query(sql_str11, [results[i].start_date, results[i].end_date, 
-                                    results[i].user_id, results[i].work], (error) => {
-                                    if (error) {
-                                        console.log(error);
-                                        res.end("error");
-                                    } else {
-                                        console.log("SUB_THIS_WORK data was inserted...");
-                                    }
-                                });
-                            }
-                        }
-                        db.query(sql_str12, (error) => {
-                            if (error) {
-                                console.log(error);
-                                res.end("error");
-                            } else {
-                               console.log("SUB_FUTURE_WORK data was deleted...");
-                            }
-                        });
+                    console.log("SUB_THIS_WORK data was deleted...");
                     }
                 });
                 callback(null);
             }
-            */
         ], function(error, result) {
             if (error)
                 console.log(error);
         }); 
-    });
+    //});
 };
 
 router.get('/backup', DataBackup);
