cb0efbddbbe4900afe61b9744705a3b62272f047
JaeHee Cho <40957996+cbw1030@users.noreply.github.com>
2020-02-26 13:36:30 +0900

    socket.io sync(chat.ejs, user_chat.js, server.js)

---
+++ routes/user_chat.js
@@ -0,0 +1,40 @@
+const   fs          = require('fs');
+const   express     = require('express');
+const   ejs         = require('ejs');
+const   mysql       = require('mysql');
+const   bodyParser  = require('body-parser');
+const   router      = express.Router();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+/* 
+    데이터베이스 연동 소스코드 
+*/
+const db = mysql.createConnection({
+    host:       'localhost',        // DB서버 IP주소
+    port:       3306,               // DB서버 Port주소
+    user:       'root',             // DB접속 아이디
+    password:   'root',             // DB암호
+    database:   'work_management'   //사용할 DB명
+});
+
+
+
+/*
+    회원 로그인 화면을 출력합니다.
+*/
+const GetChatPage = (req, res) => {
+    let chatPageHtmlStream = ''; 
+    chatPageHtmlStream = chatPageHtmlStream + fs.readFileSync(__dirname + '/../views/header.ejs','utf8'); 
+    chatPageHtmlStream = chatPageHtmlStream + fs.readFileSync(__dirname + '/../views/chat.ejs','utf8'); 
+    chatPageHtmlStream = chatPageHtmlStream + fs.readFileSync(__dirname + '/../views/footer.ejs','utf8'); 
+
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf8'}); // 200은 성공
+    res.end(ejs.render(chatPageHtmlStream, {
+                                            'title' : '로그인',
+                                            'url' : '../../' })); 
+};
+
+router.get('/', GetChatPage);
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-// Node.JS 내외부 모듈추출
+/*
+    Node.JS 내외부 모듈추출
+*/
 const   cookieParser    = require('cookie-parser');
 const   session         = require('express-session');
 const   bodyParser      = require('body-parser');
@@ -7,15 +9,24 @@ const   createError     = require('http-errors');
 const   path            = require('path');
 const   os              = require('os');
 const   app             = express();
-// BMS 개발소스 모듈
+
+/*
+    BMS 개발소스 모듈
+*/
 const   mainUI          = require('./routes/main');
 const   userWork        = require('./routes/user_work');
 const   user            = require('./routes/user');
 const   data            = require('./routes/data');
-// BMS 전용 포트주소 설정
+const   chat            = require('./routes/user_chat');
+
+/*
+    BMS 전용 포트주소 설정
+*/
 const   PORT = 3000;
 
-// 실행환경 설정부분
+/*
+    실행환경 설정부분
+*/
 app.set('views', path.join(__dirname, 'views'));  // views경로 설정(ejs파일이 있는곳을 'view'로 가리킴)
 app.set('view engine', 'ejs');                    // view엔진 지정
 app.use(express.static(path.join(__dirname, 'public')));   // public설정
@@ -29,18 +40,77 @@ app.use(session({ key: 'sid',
                   resave: false,         // 접속할때마다 id부여금지
                   saveUninitialized: true })); // 세션id사용전에는 발급금지
 
-// URI와 핸들러를 매핑
+/*
+    소켓 통신 소스코드
+*/
+io.on('connection', function(socket) {
+
+    // 접속한 클라이언트의 정보가 수신되면
+    socket.on('login', function(data) {
+        console.log('Client logged-in:\n name:' + data.name + '\n userid: ' + data.userid);
+
+        // socket에 클라이언트 정보를 저장한다
+        socket.name = data.name;
+        socket.userid = data.userid;
+
+        // 접속된 모든 클라이언트에게 메시지를 전송한다
+        io.emit('login', data.name );
+    });
+
+    // 클라이언트로부터의 메시지가 수신되면
+    socket.on('chat', function(data) {
+        console.log('Message from %s: %s', socket.name, data.msg);
+
+        var msg = {
+            from: {
+                name: socket.name,
+                userid: socket.userid},
+            msg: data.msg
+        };
+
+        // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+        socket.broadcast.emit('chat', msg);
+
+        // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+        // socket.emit('s2c chat', msg);
+
+        // 접속된 모든 클라이언트에게 메시지를 전송한다
+        // io.emit('s2c chat', msg);
+
+        // 특정 클라이언트에게만 메시지를 전송한다
+        // io.to(id).emit('s2c chat', data);
+    });
+    
+    // force client disconnect from server
+    socket.on('forceDisconnect', function() {
+        socket.disconnect();
+    })
+    
+    socket.on('disconnect', function() {
+        console.log('user disconnected: ' + socket.name);
+    });
+});
+    
+/*
+    URI와 핸들러를 매핑
+*/
 app.use('/', mainUI);             // URI (/) 접속하면 main.js로 라우팅
 app.use('/userwork', userWork);   // URI (/userwork) 접속하면 user_work.js로 라우팅
 app.use('/user', user);           // URI (/user) 접속하면 user.js로 라우팅
 app.use('/data', data);           // URI (/data) 접속하면 data.js로 라우팅
-// 서버를 실행합니다.
+app.use('/chat', chat);
+
+/*
+    서버를 실행 소스코드
+*/ 
 app.listen(PORT, function () {
     let ip_address = getServerIp();
     console.log('서버실행: http://' + ip_address +':' + PORT + '/');
 });
 
-// 서버 ip 가져오는 함수
+/*
+    서버 ip 가져오는 함수
+*/
 function getServerIp() {
     var ifaces = os.networkInterfaces();
     var result = '';
