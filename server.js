aa01b3ce9ce8e752d6401dd0240c02d132a0fa0f
JaeHee Cho <40957996+cbw1030@users.noreply.github.com>
2020-02-27 15:58:11 +0900

    채팅 프로그램 구현 완료

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const   createError     = require('http-errors');
 const   path            = require('path');
 const   os              = require('os');
 const   app             = express();
+const   http            = require('http').Server(app); //1
+const   io              = require('socket.io')(http);    //1
+
 
 /*
     BMS 개발소스 모듈
@@ -52,26 +55,32 @@ app.use('/chat', chat);           // URI (/chat) 접속하면 chat.js로 라우�
 /*
     서버 실행 소스코드
 */ 
-app.listen(PORT, function () {
+http.listen(PORT, function () {
     let ip_address = getServerIp();
     console.log('서버실행: http://' + ip_address +':' + PORT + '/');
+    
 });
 
-// 채팅 socket.io 서버 실행
-app.io = require('socket.io')();
-
-app.io.on('connection', function(socket){
-    
+/*
+    채팅 socket.io 서버 실행
+*/
+io.on('connection', function(socket) {
     socket.broadcast.emit('새로운 분이 입장하셨습니다.');
     
-    socket.on('sendmsg', function(msg){
-        app.io.emit('sendmsg', msg);
-    }); 
-
-    socket.on('disconnect', function(user){
-        console.log(user + '이(가) 나가셨습니다.');
+    // 메세지를 보낸 해당 사람에게 전송
+    socket.on('sendmsg', function(msg) {
+        socket.emit('sendmsg', msg); 
     });
-     
+
+    // 메세지를 보낸 해당 사람을 제외하고 모든사람들에게 전송
+    socket.on('sendmsg', function(msg) {
+        socket.broadcast.emit('sendmsg_broadcast', msg); 
+    });
+
+    // 연결 끊겼을 때
+    socket.on('disconnect', function() {
+        console.log(socket.name + '이(가) 나가셨습니다.');
+    });   
 });
 
 
